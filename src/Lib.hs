e4781e54aeee364f97c3b448597915df29dc1c05
John Downs <john.downs@acm.org>
2017-08-02 21:45:45 -0400

    better types from read to parse

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,48 +8,50 @@ import Data.Maybe
 import Types
 import Control.Exception
 
+readXml :: FilePath -> IO [Content]
 readXml filepath = do
   xmlContent <- readFile filepath
   return (parseXML xmlContent)
 
+parseTestCase :: [Content] -> TestCase
 parseTestCase content =
   (TestCase
     (attr name)
     (attrInt tests)
     (attr className) "" 0 [] [] [] [] [])
   where
-    element = head (onlyElems (parseXML content))
+    element = head (onlyElems content)
     name = QName "name" Nothing Nothing
     className = QName "classname" Nothing Nothing
     tests = QName "assertions" Nothing Nothing
     attrInt = (\key -> (read (fromMaybe "0" (findAttr key element))) :: Integer)
     attr = (\key -> (fromMaybe "" (findAttr key element)))
 
-parseFailure :: XmlSource t => t -> Failure
+parseFailure :: [Content] -> Failure
 parseFailure xmlContent = parse Failure xmlContent "failure"
 
-parseError :: XmlSource t => t -> Error
+parseError :: [Content] -> Error
 parseError xmlContent = parse Error xmlContent "error"
 
-parseProperty :: XmlSource t => t -> Property
+parseProperty :: [Content] -> Property
 parseProperty content =
   Property (attr name) (attr value)
   where
-    element = head (onlyElems (parseXML content))
+    element = head (onlyElems content)
     name = QName "name" Nothing Nothing
     value = QName "value" Nothing Nothing
     attr = (\key -> (fromMaybe "" (findAttr key element)))
 
-parseSkipped :: XmlSource t => t -> Skipped
+parseSkipped :: [Content] -> Skipped
 parseSkipped content = Skipped (attr message)
   where
-    element = head (onlyElems (parseXML content))
+    element = head (onlyElems content)
     message = QName "message" Nothing Nothing
     attr = (\key -> (fromMaybe "" (findAttr key element)))
 
-parse :: XmlSource t => ([Char] -> [Char] -> t1) -> t -> String -> t1
-parse t content qname = -- TODO: handle exeption if qName is wrong...
-  let em = head (onlyElems (parseXML content))
+parse :: ([Char] -> [Char] -> t) -> [Content] -> String -> t
+parse t content qname =
+  let em = head (onlyElems content)
       mq = QName "message" Nothing Nothing
       tq = QName "type" Nothing Nothing
       attr = (\name -> (fromMaybe "" (findAttr name em)))
